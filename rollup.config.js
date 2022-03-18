b275110d430c4bf990b095e792923e989aca2c67
n3vrax <n3vrax@gmail.com>
2022-03-18 12:28:38 +0200

    small improvements

--- rollup.config.js
+++ rollup.config.js
@@ -10,24 +10,30 @@ import pkg from './package.json';
 const production = !process.env.ROLLUP_WATCH;
 
 export default [
-  // browser-friendly UMD build
   {
     input: 'src/index.ts',
     output: {
-      name: 'Gamerig',
-      file: pkg.browser,
+      name: 'MyLib', // this is the name of the global variable in browser
+      file: 'dist/browser/my-lib.min.js',
       format: 'umd',
       sourcemap: !production,
+      globals: {}, // map external libraries to their brower globals
     },
+    external: [], // tell rollup to ignore bundling these libraries into the output
     plugins: [
       del({
         targets: ['./dist'],
         runOnce: !production,
       }),
-      resolve(),
+      resolve(), // resolve imports and bundles them in the output file
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-      production ? terser() : undefined,
+      typescript({ tsconfig: './tsconfig.json', declaration: true, declarationDir: 'dts' }),
+      production &&
+        terser({
+          format: {
+            comments: false,
+          },
+        }),
     ],
   },
 
@@ -39,15 +45,16 @@ export default [
       { file: pkg.main, format: 'cjs', sourcemap: !production },
       { file: pkg.module, format: 'es', sourcemap: !production },
     ],
+    external: [], // tell rollup to ignore bundling these libraries into the output
   },
   // bundle all type definitions into one file
   {
-    input: 'dist/dts/index.d.ts',
+    input: 'dist/browser/dts/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'es' }],
     plugins: [
       dts(),
       del({
-        targets: ['./dist/dts'],
+        targets: ['./dist/browser/dts'],
         hook: 'buildEnd',
       }),
     ],
